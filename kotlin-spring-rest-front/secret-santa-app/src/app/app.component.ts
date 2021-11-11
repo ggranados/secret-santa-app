e11ad5161130f2c61e28ab4d266048a99de7dc68
Guillermo Granados <guillermo.granados@outlook.com>
2021-11-11 20:07:03 -0300

    static events loading

--- kotlin-spring-rest-front/secret-santa-app/src/app/app.component.ts
+++ kotlin-spring-rest-front/secret-santa-app/src/app/app.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms'
+import { HttpClient } from '@angular/common/http'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'secret-santa-app';
+
+  constructor(private http:HttpClient){}
+
+  private baseUrl: string = 'http://localhost:8080';
+  private eventsUrl: string = this.baseUrl + 'api/v1/events/';
+
+  events: Event[] = [];
+
+  ngOnInit(){
+    this.events = [ 
+      new Event("1", "Guadalupe", "12/12/2021"),
+      new Event("2", "Navidad", "31/12/2021"),
+      new Event("3", "Reyes Magos", "06/01/2022") 
+    ];
+  }
+}
+
+
+export class Event {
+  id: string;
+  title: string;
+  date: string;
+
+  constructor(id: string, title: string, date: string){
+    this.id = id;
+    this.title = title;
+    this.date = date;
+  }
 }
